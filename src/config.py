0f1cd3fe39ea9f58a5cd029471585ed06f2f1850
Janusz Przewocki <janusz@lifeconcept.tech>
2025-02-24 13:16:04 +0100

    Prepared dataset read code + config

---
+++ src/config.py
@@ -0,0 +1,1 @@
+label_column = 'acc_gyro_event'
--- src/datasets.py
+++ src/datasets.py
@@ -1,17 +1,13 @@
 # Standard library imports
 from abc import ABC, abstractmethod
-from datetime import timedelta
-import os
 
 # Local imports
-# from datadownloader.indexer import recordings_file, get_acc_data
-# from tools.utils import timestamp_to_time, get_repo_path
-# from config import Patients
-# import config
+from utils import get_repo_path
+import config as cfg
 
 # Third-party imports
-# import pandas as pd
-# import numpy as np
+import pandas as pd
+import numpy as np
 
 
 class Dataset(ABC):
@@ -27,6 +23,6 @@ class LaboratoryData(Dataset):
 
         df_acc = pd.read_pickle(get_repo_path() / 'datasets/data_raw.pkl')
 
-        df_acc[config.General.label_column] = df_acc[config.General.label_column].apply(lambda x: x.astype(int))
+        df_acc[cfg.label_column] = df_acc[cfg.label_column].apply(lambda x: x.astype(int))
 
         return df_acc
